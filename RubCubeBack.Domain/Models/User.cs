9ec392179ff57839160213bd4b2561f9af8036a5
Victor <dartinjs@gmail.com>
2026-02-13 09:28:35 -0300

    Adding domain layer and user model

---
+++ RubCubeBack.Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubCubeBack.Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- RubCubeBack.Infra/Context/RubAppDbContext.cs
+++ RubCubeBack.Infra/Context/RubAppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RubCubeBack.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,5 +12,7 @@ namespace RubCubeBack.Infra.Context
         {
             
         }
+
+        public DbSet<User> Users { get; set; }
     }
 }
