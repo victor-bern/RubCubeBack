eba5f6116e45aff9e43b02ff9f661064a775b9f1
Victor <dartinjs@gmail.com>
2026-02-14 13:10:06 -0300

    add Logs Dto folder

---
+++ RubCubeBack.Application/DTOs/Logs/LogsFilterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubCubeBack.Application.DTOs.Log
+{
+    public record LogFilterDTO
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? UserId { get; set; }
+        public DateTime? StartAt { get; set; }
+        public DateTime? EndAt { get; set; }
+     
+        public string? Endpoint { get; set; }
+    }
+}
